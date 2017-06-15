659946564b50a270891eb52465ebabb9e6836ba8
Rich FitzJohn <rich.fitzjohn@gmail.com>
2017-06-15 17:14:25 +0100

    Code in the required git version

--- R/git.R
+++ R/git.R
@@ -1,14 +1,25 @@
 download_git <- function(path, progress = NULL) {
-  url <- "https://api.github.com/repos/git-for-windows/git/releases/latest"
-  r <- curl::curl_fetch_memory(url)
-  dat <- jsonlite::fromJSON(rawToChar(r$content))
-  re <- "^Git-(.*)-64-bit.exe"
-  i <- grep(re, dat$assets$name)
-  if (length(i) != 1L) {
-    stop("Unexpected response from github release") # nocov
-  }
-  label <- sprintf("git (%s)", sub(re, "\\1", dat$assets$name[i]))
-  provisionr::download_files(dat$assets$browser_download_url[i],
-                             path, labels = label, count = FALSE,
-                             progress = progress)
+  version <- "2.13.1"
+  root <- "https://github.com/git-for-windows/git/releases/download/"
+  release <- sprintf("v%s.windows.1", version)
+  filename <- sprintf("Git-%s-64-bit.exe", version)
+  url <- file.path(root, release, filename)
+
+  ## It is somewhat possible to fetch the most recent relase but it's
+  ## flakey in tests and there are other things mixed in with the
+  ## releases that look like it might make this a bit unreliable.  For
+  ## the record though, this does work in theory, if not in practice:
+  ##
+  ##   url <- "https://api.github.com/repos/git-for-windows/git/releases/latest"
+  ##   r <- curl::curl_fetch_memory(url)
+  ##   dat <- jsonlite::fromJSON(rawToChar(r$content))
+  ##   re <- "^Git-(.*)-64-bit.exe"
+  ##   i <- grep(re, dat$assets$name)
+  ##   if (length(i) != 1L) {
+  ##     stop("Unexpected response from github release")
+  ##   }
+  ##   url <- dat$assets$browser_download_url[i]
+  ##   version <- sub(re, "\\1", dat$assets$name[i])
+  provisionr::download_files(url, path, labels = sprintf("git (%s)", version),
+                             count = FALSE, progress = progress)
 }
